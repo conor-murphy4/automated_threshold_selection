cb8156fa435c45f6a39124af24f1cdb06c3169f4
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2024-01-18 17:10:13 +0000

    tidy script

--- simulation_study/plot-case-study-denisties.R
+++ simulation_study/plot-case-study-denisties.R
@@ -4,7 +4,12 @@
 
 source("helper_functions.R") # for GPD density and quantiles
 
-density_discontinuous <- function(x, uniform_lower, uniform_upper, proportion_uniform, gpd_scale, gpd_shape){
+density_discontinuous <- function(x,
+    uniform_lower = 0.5,
+    uniform_upper = 1.0, 
+    proportion_uniform = 1/6,
+    gpd_scale = 0.5,
+    gpd_shape = 0.1){
 
   is_undefined <- which(x < uniform_lower)
   is_uniform <- which((x > uniform_lower) & (x < uniform_upper))
@@ -18,7 +23,7 @@ density_discontinuous <- function(x, uniform_lower, uniform_upper, proportion_un
   return(density)
 }
 
-density_continuous <- function(x, alpha = 1, beta = 0.5){
+density_continuous <- function(x, alpha = 1, beta = 2){
   gpd_scale = 0.5
   gpd_shape = 0.1
   
@@ -39,29 +44,6 @@ density_continuous <- function(x, alpha = 1, beta = 0.5){
   return(density)
 }
 
-density_continuous_2 <- function(x, alpha = 1, beta = 0.5){
-  gpd_scale = 0.5
-  gpd_shape = 0.1
-  
-  is_undefined <- which(x < 0)
-  is_bgpd <- which((x > 0) & (x < 1))
-  is_gpd <- which(x > 1)
-
-  # probability density up to proportionality (integral less than 1)
-  prop_to_density <- function(x, a = alpha, b = beta){
-    dgpd(x, shape = gpd_shape, scale = gpd_scale) * pbeta(x, a, b)
-  } 
-  
-  # Calculate area under the curve between 0 and 1
-  q <- integrate(f = prop_to_density, lower = 0, upper = 1)$value
-
-  # scale conditional distribution  
-  density <- prop_to_density(x)
-  density[is_gpd] <- (1 - q) * dgpd(x[is_gpd] - 1, shape = 0.1, scale = 0.6, mu = 0)
-
-  return(density)
-}
-
 
 #############################################
 # Evaluate probability density functions ----
@@ -69,120 +51,44 @@ density_continuous_2 <- function(x, alpha = 1, beta = 0.5){
 
 t <- seq(0, 4, length.out = 5001)
 
-case_1_density <- density_discontinuous(
-  x = t,
-  uniform_lower = 0.5,
-  uniform_upper = 1.0,
-  proportion_uniform = 1/6,
-  gpd_scale = 0.5,
-  gpd_shape = 0.1
+densities_by_case <- list(
+  case_1 = density_discontinuous(x = t, gpd_shape = 0.1),
+  case_2 = density_discontinuous(x = t, gpd_shape = 0.1),   # same as case 1
+  case_3 = density_discontinuous(x = t, gpd_shape = -0.05), 
+  case_4 = density_continuous(x = t)
 )
-
-case_3_density <- density_discontinuous(
-  x = t,
-  uniform_lower = 0.5,
-  uniform_upper = 1.0,
-  proportion_uniform = 1/6,
-  gpd_scale = 0.5,
-  gpd_shape = -0.05
-)
-
-case_4_density_1 <- density_continuous(t)
-case_4_density_1a <- density_continuous(t, beta = 2)
-case_4_density_2 <- density_continuous_2(t)
 
 ###################
 # Create Plots ----
 ###################
 
-pdf("simulation_study/case_study_plots.pdf", width = 7, height = 5)
+pdf("simulation_study/case_study_denisty_plots.pdf", width = 7, height = 5)
 par(oma = c(0.1,0.1,0.1,0.1), mar=c(4.5, 4.5, 1, 1))
-plot(
-  x = t,
-  y = case_1_density,
-  type = "l",
-  lwd = 2,
-  cex.axis = 1.75,
-  cex.lab = 1.75,
-  xlab = "x",
-  ylab = "probability density",
-  bty = "n",
-  ylim = c(0,2),
-  #main = "Cases 1 and 2",
+for (density_values in densities_by_case){
+  plot(
+    x = t,
+    y = density_values,
+    type = "l",
+    lwd = 2,
+    cex.axis = 1.75,
+    cex.lab = 1.75,
+    xlab = "x",
+    ylab = "probability density",
+    bty = "n",
+    ylim = c(0,2),
+    #main = "Cases 1 and 2",
   )
-abline(v = 1, lty = 2, lwd = 2)
-
-plot(
-  x = t,
-  y = case_3_density,
-  type = "l",
-  lwd = 2,
-  cex.axis = 1.75,
-  cex.lab = 1.75,
-  xlab = "x",
-  ylab = "probability density",
-  bty = "n",
-  ylim = c(0,2),
-  #main = "Case 3",
-  )
-abline(v = 1, lty = 2, lwd = 2)
-
-plot(
-  x = t,
-  y = case_4_density_1,
-  type = "l",
-  lwd = 2,
-  cex.axis = 1.75,
-  cex.lab = 1.75,
-  xlab = "x",
-  ylab = "probability density",
-  bty = "n",
-  ylim = c(0,2),
-  #main = "Case 4 - ZV method beta = 0.5",
-  )
-abline(v = 1, lty = 2, lwd = 2)
-
-plot(
-  x = t,
-  y = case_4_density_2,
-  type = "l",
-  lwd = 2,
-  cex.axis = 1.75,
-  cex.lab = 1.75,
-  xlab = "x",
-  ylab = "probability density",
-  bty = "n",
-  ylim = c(0,2),
-  #main = "Case 4 - JT method beta = 0.5",
-  )
-abline(v = 1, lty = 2, lwd = 2)
-
-plot(
-  x = t,
-  y = case_4_density_1a,
-  type = "l",
-  lwd = 2,
-  cex.axis = 1.75,
-  cex.lab = 1.75,
-  xlab = "x",
-  ylab = "probability density",
-  bty = "n",
-  ylim = c(0,2),
-  #main = "Case 4 - ZV method beta = 2",
-  )
-abline(v = 1, lty = 2, lwd = 2)
+  abline(v = 1, lty = 2, lwd = 2)
+}
 dev.off()
 
 
 ## Tests ----------------------------------------------------------------
 
-# Sanity check against sample proportion below threshold
+# Sanity check against sample proportion below threshold, (approx 0.721)
 
-integrate(density_continuous, lower = 0, upper = 1) # matches sample
+integrate(density_continuous, lower = 0, upper = 1)
 integrate(density_continuous, lower = 0, upper = 100)
-
-integrate(density_continuous_2, lower = 0, upper = 1) # does not match sample
-integrate(density_continuous_2, lower = 0, upper = 100)
 
 # Extend over all quantiles
 
@@ -190,38 +96,56 @@ integrate(density_continuous_2, lower = 0, upper = 100)
 set.seed(4321)
 n_sim <- 1e6
 Y <- threshold:::rgpd(n = n_sim, shape = 0.1, scale = 0.5)
-B <- rbeta(n = n_sim, shape1 = 1, shape2 = 0.5)
+B <- rbeta(n = n_sim, shape1 = 1, shape2 = 2)
 X <- Y[Y>B]
+tau <- mean(X < 1)
 
-## Define model and sample cdfs (slow but correct)
+## Define model and sample cdfs
 
 model_cdf   <- function(to){integrate(density_continuous, 0, to)$value}
-model_cdf_2 <- function(to){integrate(density_continuous_2, 0, to)$value}
 sample_cdf  <- function(to){mean(X <= to)}
 
 ## Compare CDFs directly
 eval_points <- 8 * ppoints(1001)
 cdf_model   <- purrr::map_dbl(eval_points, .f = model_cdf)
-cdf_model_2 <- purrr::map_dbl(eval_points, .f = model_cdf_2)
 cdf_sample  <- purrr::map_dbl(eval_points, .f = sample_cdf)
 
 # Overlay CDFs
-pdf("cdf-comparison.pdf", width = 7, height = 5)
-plot(eval_points, cdf_model, type = "l", lwd = 3, col = "darkorange")
-lines(eval_points, cdf_model_2, lwd = 3, col = "purple")
+pdf("simulation_study/cdf_check.pdf", width = 7, height = 5)
+plot(
+  x = eval_points,
+  y = cdf_model, 
+  xlab = "x",
+  ylab = "CDF", 
+  type = "l",
+  lwd = 3,
+  col = "darkorange")
 lines(eval_points, cdf_sample, col = "black", lty = 2)
 legend(
   "bottomright",
-  legend = c("ZV", "JT", "Sample"),
+  legend = c("Model", "Empirical"),
   lwd = 2,
-  lty = c(1,1,2),
-  col = c("darkorange", "purple", "black")
+  lty = c(1,2),
+  col = c("darkorange", "black")
 )
 dev.off()
 
 ## QQ plot
-pdf("qq-plot-comparison.pdf", width = 5, height = 5)
-plot(x = cdf_sample, y = cdf_model, col = "darkorange",  xlab = "sample quantile", ylab = "model quantile")
-points(x = cdf_sample, y = cdf_model_2, col = "purple")
-abline(a = 0 , b = 1, lwd = 2, col = "black")
+pdf("simulation_study/qq_plot_check.pdf", width = 5, height = 5)
+plot(
+  x = cdf_sample, 
+  y = cdf_model, 
+  col = "darkorange", 
+  type = "l", 
+  lwd = "3",
+  xlab = "sample quantile", 
+  ylab = "model quantile")
+abline(a = 0 , b = 1, lwd = 1, col = "black")
+legend(
+  "bottomright",
+  legend = c("Model", "Empirical"),
+  lwd = 2,
+  lty = c(1,2),
+  col = c("darkorange", "black")
+)
 dev.off()
