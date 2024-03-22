39b7c3cb664ff6679592a74297a8f51daa482c0e
Murphy <murphyc4@lancaster.ac.uk>
2024-03-22 14:00:27 +0000

    Updated src files, changed function names to eqd, added output files for eqd in cases 1-4

--- src/eqd_mnu.R
+++ src/eqd_mnu.R
@@ -1,18 +1,18 @@
 # Load helper functions for working with the generalised Pareto distribution
-source('helper_functions.R')
+source('src/helper_functions.R')
 
 #-----------------------------------------------------------------------
 
 #' Threshold selection method for univariate extremes
 #'
-#' 'thresh_qq_metric' selects a constant threshold above which the data can be most closely modelled by a Generalised Pareto distribution.
+#' 'eqd_mnu' is a variant of the 'eqd' function which selects a constant threshold above which the data can be most closely modelled by a Generalised Pareto distribution. In this variant, the number of equally-spaced probabilities on which the quantile function is evaluated are allowed to vary with the number of excesses of each candidate threshold.
 #'
 #' @author Conor Murphy
 #'
 #' @param data A numeric vector.
 #' @param thresh A numeric vector of proposed thresholds to test.
 #' @param k  A positive integer denoting the number of bootstraps.
-#' @param m A positive integer denoting the number of equally-spaced probabilities at which to evaluate quantiles.
+#' @param c A positive number denoting the constant of proportionality of the number of equally-spaced probabilities m to the number of excesses n_u, i.e., m=cn_u.
 #'
 #' @returns A list containing the chosen threshold, the parameters of the fitted GPD, the number of observations above the chosen thresholds and the metric values 'd' corresponding to each proposed threshold.
 #'
@@ -20,7 +20,7 @@ source('helper_functions.R')
 #' set.seed(12345)
 #' data_test1 <- rgpd(1000, shape = 0.1, scale=0.5, mu=1)
 #' thresholds1 <- quantile(data_test1,seq(0,0.95,by=0.05))
-#' (example1 <- thresh_qq_metric(data_test1, thresh = thresholds1))
+#' (example1 <- eqd_mnu(data_test1, thresh = thresholds1))
 #'
 #' set.seed(11111)
 #' test2 <- rgpd(10000, shape = 0.1, scale=0.5)
@@ -29,16 +29,16 @@ source('helper_functions.R')
 #' keep <- test2>cens_thr
 #' data_test2 <- test2[keep]
 #' thresholds2 <- quantile(data_test2,seq(0, 0.95, by=0.05))
-#' (example2 <- thresh_qq_metric(data_test2,thresh = thresholds2))
+#' (example2 <- eqd_mnu(data_test2,thresh = thresholds2))
 
 
-thresh_qq_metric_Qpx <- function(data, thresh, k = 100, m = 500){
+eqd_mnu <- function(data, thresh, k = 100, c = 1){
 
   # Check inputs are valid
   if (!is.numeric(data)) stop("Data must be a vector")
   if (!is.numeric(thresh)) stop("u to be tested needs to be a vector")
   if (k <= 0 | k %% 1 != 0) stop("Number of bootstrapped samples must be a positive integer")
-  if (m <= 0 | m %% 1 != 0) stop("Number of equally spaced probabilities must be a positive integer")
+  #if (m <= 0 | m %% 1 != 0) stop("Number of equally spaced probabilities must be a positive integer")
 
   meandistances <- xis <- sigmas <- num_excess <- numeric(length(thresh))
   for (i in 1:length(thresh)) {
@@ -46,6 +46,7 @@ thresh_qq_metric_Qpx <- function(data, thresh, k = 100, m = 500){
     excess <- data[data > u] - u
     num_excess[i] <- length(excess)
     if (num_excess[i] > 10) {
+      m <- round(c*num_excess[i])
       mle0 <- mean(excess)
       init.fit <- optim(GPD_LL, z = excess, par = c(mle0,0.1), control = list(fnscale = -1))
       xis[i] <- init.fit$par[[2]]
@@ -57,7 +58,7 @@ thresh_qq_metric_Qpx <- function(data, thresh, k = 100, m = 500){
         ifelse(xis[i] < 0, pars_init <-  c(mle, 0.1) ,pars_init <- c(sigmas[i], xis[i]) )
         gpd.fit <- optim(GPD_LL, z = X, par = pars_init, control = list(fnscale = -1))
         quants <- qgpd((1:m) / (m + 1), scale = gpd.fit$par[[1]], shape = gpd.fit$par[[2]])
-        distances[j] <- (1 / m) * sum(abs(quantile(excess, probs = (1:m) / (m+1)) - quants))
+        distances[j] <- (1 / m) * sum(abs(quantile(X, probs = (1:m) / (m+1)) - quants))
       }
       meandistances[i] <- mean(distances)
     }
