b1e10523628f9ceed93e2dc5a1563428cce59c9e
Murphy <murphyc4@lancaster.ac.uk>
2024-09-10 10:44:07 +0100

    Removed highest threshold for Nidd due to numerical issues

--- Section 2/Figure_1_example_parameter_stability_plots.R
+++ Section 2/Figure_1_example_parameter_stability_plots.R
@@ -5,18 +5,19 @@ case_4_sample_1 <- Case_4[,1]
 
 data("nidd.thresh")
 
-Q <-  seq(0,0.95,by=0.05)
-
 #Candidate threshold grids
-thresh_4 <- quantile(case_4_sample_1, Q, names=FALSE)
-thresh_Nidd <- quantile(nidd.thresh,Q, names=F)
+Q_4 <- seq(0,0.95,by=0.05)
+thresh_4 <- quantile(case_4_sample_1, Q_4, names=FALSE)
+
+Q_Nidd <- seq(0,0.95,by=0.04)
+thresh_Nidd <- quantile(nidd.thresh,Q_Nidd, names=F)
 
 #Plotting window
 dev.new(width=9.17, height=3.7,noRStudioGD = TRUE)
 par(mfrow=c(1,2),bg='transparent')
 
 #Parameter stability plots
-shapestabboot(case_4_sample_1, thresholds = thresh_4, Q=Q, reverse=F, boot=TRUE,m.boot=200)
+shapestabboot(case_4_sample_1, thresholds = thresh_4, Q=Q_4, reverse=F, boot=TRUE,m.boot=200)
 abline(v=1.0, col="green")
 
-shapestabboot(nidd.thresh, thresholds = thresh_Nidd, Q=Q, reverse=F, boot=TRUE,m.boot=200)
+shapestabboot(nidd.thresh, thresholds = thresh_Nidd, Q=Q_Nidd, reverse=F, boot=TRUE,m.boot=200)
